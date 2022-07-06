472a49e9dc5d4bde606f96f05026748c4f714b7e
Luke Annison <lukeannison@gmail.com>
2022-07-06 19:10:20 +0100

    updated next webpack config

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -1,17 +1,39 @@
+const path = require('path');
+
 module.exports = (phase, { defaultConfig }) => {
   return {
     ...defaultConfig,
     reactStrictMode: true,
 
     webpack: (config) => {
-      config.resolve = {
+      (config.resolve = {
         ...config.resolve,
+        extensions: [".tsx", ".ts", ".js"],
         fallback: {
           fs: false,
           path: false,
           os: false,
         },
-      };
+      }),
+        (config.entry = {
+          popup: "./next-app/pages/index.tsx",
+        //   background: "./src/background/index.tsx",
+        //   content: "./src/content/index.tsx",
+        }),
+        (config.mode = "development"),
+        (config.module = {
+          rules: [
+            {
+              use: "ts-loader",
+              test: /\.tsx?$/,
+              exclude: /node_modules/,
+            },
+          ],
+        }),
+        (config.output = {
+          filename: "bundle.js",
+          path: path.resolve(__dirname, "dist"),
+        });
       return config;
     },
   };
