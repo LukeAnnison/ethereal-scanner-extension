1f25f66b985dfcaafbd6d0908bee8479526c85c1
Luke Annison <lukeannison@gmail.com>
2022-07-06 19:12:35 +0100

    initial commit

--- utils/getHashtags.ts
+++
@@ -1,9 +0,0 @@
-const getHashtags = (description: string) => {
-    if (description != null) {
-        //@ts-ignore
-        return description.match(/#\w+/g).map((x: string) => x.substring(1));
-
-    }
-};
-
-export default getHashtags;
--- utils/linkTruncate.ts
+++
@@ -1,8 +0,0 @@
-const linkTruncate = (link: string) => {
-  const searchTerm = "video/";
-  const indexOfFirst = link.indexOf(searchTerm);
-
-  return  link.slice(indexOfFirst + 6, indexOfFirst + 25);
-};
-
-export default linkTruncate;
